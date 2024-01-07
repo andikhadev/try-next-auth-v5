07f9b7042678a3b2a38547b56cb36478469b12e6
Andikha Dian <andikha.nugraha@manuva.com>
2024-01-07 14:07:51 +0700

    Added sign in with credential provider and sign out functionality with next auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,32 @@
-import GitHub from 'next-auth/providers/github'
-
 import type { NextAuthConfig } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+import { LoginSchema } from './schemas'
+import { getUserByEmail } from './data/user'
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+        if (!validatedFields.success) {
+          throw new Error('Invalid credentials')
+        }
+
+        const { email, password } = validatedFields.data
+
+        const user = await getUserByEmail(email)
+        if (!user || !user.password) {
+          throw new Error('User not found')
+        }
+
+        const matchedPassword = await bcrypt.compare(password, user.password)
+        if (!matchedPassword) {
+          throw new Error('Your password is incorrect')
+        }
+
+        return user
+      },
+    }),
+  ],
 } satisfies NextAuthConfig
